5f86a5dc26d56b8469fb3dee9ded5b81ba956ef0
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-10-31 10:34:59 +0100

    Bug fix for average speed (1.4): division by 0

--- monolith/views/home.py
+++ monolith/views/home.py
@@ -24,7 +24,8 @@ def index():
         total_average_speed = 0
         for run in runs:
             total_average_speed += run.average_speed
-        total_average_speed /= runs.count()
+        if runs.count():
+            total_average_speed /= runs.count()
     else:
         runs = None
         total_average_speed = 0
