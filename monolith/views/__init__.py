55f6c5ed92ccaa8de909d8148e614ee6cb04d28a
Albertomac <albertomac@live.it>
2018-11-02 11:39:52 +0100

    Initial setup

--- monolith/views/__init__.py
+++ monolith/views/__init__.py
@@ -2,6 +2,7 @@ from .home import home
 from .auth import auth
 from .users import users
 from .strava import strava
+from .runs import runs
 
 
-blueprints = [home, auth, users, strava]
+blueprints = [home, auth, users, strava, runs]
