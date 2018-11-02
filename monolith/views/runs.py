13931784554b01f2541bc536695ed0c249ab181a
Albertomac <albertomac@live.it>
2018-11-02 12:58:52 +0100

    run endpoint completed

--- monolith/views/runs.py
+++ monolith/views/runs.py
@@ -6,7 +6,19 @@ from monolith.auth import current_user, login_required
 runs = Blueprint('runs', __name__)
 
 
-@runs.route('/runs/<id>')
-def _runs(id):
-    run = None
-    return render_template("runs.html", run=run)
+@runs.route('/runs/<id>', methods=['GET'])
+@login_required
+def run(id):
+    name    = None
+    date    = None
+    headers = None
+    values  = None
+
+    run = db.session.query(Run).filter(Run.runner_id == current_user.id, Run.id == id).first()
+    if run is not None:
+        name    = run.name
+        date    = run.start_date.strftime('%A %d/%m/%y at %H:%M')
+        headers = ['Distance (m)', 'AVG Speed (m/s)', 'Elapsed Time (s)', 'Elevation (m)']
+        values  = [run.distance, run.average_speed, run.elapsed_time, run.total_elevation_gain]
+    
+    return render_template("runs.html", name=name, date=date, headers=headers, values=values)
