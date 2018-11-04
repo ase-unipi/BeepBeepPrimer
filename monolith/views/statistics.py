7894bf991f65cae9df930030424e0496c2d11991
DanyEle <daniele.gadler@yahoo.it>
2018-11-04 14:07:06 +0100

    Fixed 500 error arising when user doesn't have any runs or has not connected the Strava account

--- monolith/views/statistics.py
+++ monolith/views/statistics.py
@@ -34,8 +34,6 @@ def stats():
     total_elevation_gains = []
     elapsed_times = []
 
-
-
     #get statistics of all runs of current user
     if current_user is not None and hasattr(current_user, 'id'):
         runs = db.session.query(Run).filter(Run.runner_id == current_user.id)
@@ -48,30 +46,28 @@ def stats():
                 total_elevation_gains.append(run.total_elevation_gain)
                 elapsed_times.append(run.elapsed_time)
                 run_names.append(run.name)
-
                 if(run.average_heartrate != None):
                     average_heartrates.append(run.average_heartrate)
-
-
                 #saving into lists completed, now let's get plotting!
                 #need to save plots on disk before actually showing them in the page!
 
                 x_label = "Run Name"
-
                 distance_plot_filename = create_plot(run_names, distances, x_label, "Distance (m)", "Distance of your last 10 runs", 'distance', 'blue', 1)
-
                 avg_speed_plot_filename = create_plot(run_names, average_speeds, x_label, "Average Speed (Km/h)", "Average speed of your last 10 runs", 'average_speed', 'g', 2)
-
                 avg_heartrate_plot_filename = create_plot(run_names, average_heartrates, x_label, "Heartrate (hrpm)", "Average Heartrate of your last 10 runs", 'average_heartrate', 'orange', 2)
-
                 elapsed_times_plot_filename = create_plot(run_names, elapsed_times, x_label, "Elapsed time (s)", "Duration time of your last 10 runss", 'elapsed_time', 'red', 2)
-
                 elevation_gain_filename = create_plot(run_names, total_elevation_gains, x_label, "Total Elevation Gain (m)", "Total elevation gain of your last 10 runs", 'elevation_gain', 'brown', 2)
 
-
-
         else:
+            #user doesn't have any runs or has not connected the Strava account
             runs = None
+
+            distance_plot_filename = None
+            avg_speed_plot_filename = None
+            avg_heartrate_plot_filename = None
+            elapsed_times_plot_filename = None
+            elevation_gain_filename = None
+
     return render_template("statistics.html", runs=runs, plots_directory=PLOTS_DIRECTORY, distance_plot_filename=distance_plot_filename,
         avg_speed_plot_filename=avg_speed_plot_filename, avg_heartrate_plot_filename=avg_heartrate_plot_filename,
         elapsed_times_plot_filename=elapsed_times_plot_filename, elevation_gain_filename=elevation_gain_filename )
@@ -82,6 +78,7 @@ def stats():
 #          = 2 --> bar plot
 def create_plot(x_values, y_values, xlabel, ylabel, title, filename, color, plot_type):
     filename_output = None
+
     if len(y_values) > 0:
         if plot_type == 1:
             plt.plot(x_values, y_values, color=color)
