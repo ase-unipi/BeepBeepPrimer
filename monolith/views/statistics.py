1041c0a220000314d1252f9c74610d6c96fe5359
DanyEle <daniele.gadler@yahoo.it>
2018-11-04 15:30:00 +0100

    Added ID integration in the plots' generation function in order to prevent different users' plots from overlapping

--- monolith/views/statistics.py
+++ monolith/views/statistics.py
@@ -34,6 +34,10 @@ def stats():
     total_elevation_gains = []
     elapsed_times = []
 
+
+    runner_id = current_user.id
+
+
     #get statistics of all runs of current user
     if current_user is not None and hasattr(current_user, 'id'):
         runs = db.session.query(Run).filter(Run.runner_id == current_user.id)
@@ -52,11 +56,11 @@ def stats():
                 #need to save plots on disk before actually showing them in the page!
 
                 x_label = "Run Name"
-                distance_plot_filename = create_plot(run_names, distances, x_label, "Distance (m)", "Distance of your last 10 runs", 'distance', 'blue', 1)
-                avg_speed_plot_filename = create_plot(run_names, average_speeds, x_label, "Average Speed (Km/h)", "Average speed of your last 10 runs", 'average_speed', 'g', 2)
-                avg_heartrate_plot_filename = create_plot(run_names, average_heartrates, x_label, "Heartrate (hrpm)", "Average Heartrate of your last 10 runs", 'average_heartrate', 'orange', 2)
-                elapsed_times_plot_filename = create_plot(run_names, elapsed_times, x_label, "Elapsed time (s)", "Duration time of your last 10 runss", 'elapsed_time', 'red', 2)
-                elevation_gain_filename = create_plot(run_names, total_elevation_gains, x_label, "Total Elevation Gain (m)", "Total elevation gain of your last 10 runs", 'elevation_gain', 'brown', 2)
+                distance_plot_filename = create_plot(runner_id, run_names, distances, x_label, "Distance (m)", "Distance of your last 10 runs", 'distance', 'blue', 1)
+                avg_speed_plot_filename = create_plot(runner_id, run_names, average_speeds, x_label, "Average Speed (Km/h)", "Average speed of your last 10 runs", 'average_speed', 'g', 2)
+                avg_heartrate_plot_filename = create_plot(runner_id, run_names, average_heartrates, x_label, "Heartrate (hrpm)", "Average Heartrate of your last 10 runs", 'average_heartrate', 'orange', 2)
+                elapsed_times_plot_filename = create_plot(runner_id, run_names, elapsed_times, x_label, "Elapsed time (s)", "Duration time of your last 10 runss", 'elapsed_time', 'red', 2)
+                elevation_gain_filename = create_plot(runner_id, run_names, total_elevation_gains, x_label, "Total Elevation Gain (m)", "Total elevation gain of your last 10 runs", 'elevation_gain', 'brown', 2)
 
         else:
             #user doesn't have any runs or has not connected the Strava account
@@ -70,13 +74,14 @@ def stats():
 
     return render_template("statistics.html", runs=runs, plots_directory=PLOTS_DIRECTORY, distance_plot_filename=distance_plot_filename,
         avg_speed_plot_filename=avg_speed_plot_filename, avg_heartrate_plot_filename=avg_heartrate_plot_filename,
-        elapsed_times_plot_filename=elapsed_times_plot_filename, elevation_gain_filename=elevation_gain_filename )
+        elapsed_times_plot_filename=elapsed_times_plot_filename, elevation_gain_filename=elevation_gain_filename,
+        runner_id=runner_id)
 
 
 
 #plot_type = 1 --> line plot
 #          = 2 --> bar plot
-def create_plot(x_values, y_values, xlabel, ylabel, title, filename, color, plot_type):
+def create_plot(runner_id, x_values, y_values, xlabel, ylabel, title, filename, color, plot_type):
     filename_output = None
 
     if len(y_values) > 0:
@@ -85,7 +90,7 @@ def create_plot(x_values, y_values, xlabel, ylabel, title, filename, color, plot
         elif plot_type == 2:
             plt.bar(x_values, y_values, color=color)
 
-        filename_output = "plots/" + filename + PLOTS_FORMAT
+        filename_output = "plots/" + filename + "_" + str(runner_id) + PLOTS_FORMAT
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
         plt.title(title)
