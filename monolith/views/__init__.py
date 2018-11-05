f692f56693252da2ba5bc371b9cd5d4bc713f32a
daquinoaldo <daquino.aldo@gmail.com>
2018-11-05 16:55:13 +0100

    Run details (template for 2.1)

--- monolith/views/__init__.py
+++ monolith/views/__init__.py
@@ -2,6 +2,7 @@ from .home import home
 from .auth import auth
 from .users import users
 from .strava import strava
+from .run import run
 
 
-blueprints = [home, auth, users, strava]
+blueprints = [home, auth, users, strava, run]
--- monolith/views/run.py
+++ monolith/views/run.py
@@ -1,15 +1,13 @@
 from flask import Blueprint, render_template
-from stravalib import Client
+from flask_login import login_required
 
 from monolith.database import db, Run
-from monolith.auth import current_user
+
+run = Blueprint('run', __name__)
 
 
-@run.route('/run:<id>',methods=['GET'])
+@run.route('/run/<id>', methods=['GET'])
+@login_required
 def get_run(id):
-     if request.method == 'GET': 
-        run = db.session.query(Run).filter(Run.id == id)
-        return render_template("run.html", run=run)
-
-
-	
+    run = db.session.query(Run).filter(Run.id == id)
+    return render_template("run.html", run=run)
