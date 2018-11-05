a0fda4007388f6ebd96d3f7369cee37de4e7f73d
lapotolo <lapotoloni@gmail.com>
2018-11-05 19:36:49 +0100

    test_set_objective proto2

--- monolith/tests/test_set_objective.py
+++ monolith/tests/test_set_objective.py
@@ -1,15 +1,18 @@
 from monolith.tests.utility import client, login, new_user
-from monolith.database import db, User
+from monolith.database import db, User, Objective
 
 def test_set_objective(client):
     tested_app, app = client
-    
-    reply = new_user(tested
-    assert = reply.status_
 
-    # doing a GET create_objective returns the template 
-    # create objective  without having logged in 
-    reply = tested_app.get('/create_objective')
+    # create a new user
+    reply = new_user(tested_app)
+    assert reply.status_code == 200
+    
+    # test for create_obective not logged in
+    reply = tested_app.post('/create_objective')
     assert reply.status_code == 401
+
+    reply = login(tested_app, email='marco@prova.it', password='123456')
+    assert reply.status_code == 200
 
     
