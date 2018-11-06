a2caa85e6af253956f8c889706bb010aef886a67
azecoder <tabriz.haji@gmail.com>
2018-11-06 20:42:54 +0100

    fix previous runs after delete activity

--- monolith/views/runs.py
+++ monolith/views/runs.py
@@ -8,7 +8,9 @@ runs = Blueprint('runs', __name__)
 @runs.route('/runs/<int:run_id>')
 def _runs(run_id):
     if current_user is not None and hasattr(current_user, 'id'):
-        runs = db.session.query(Run).filter(Run.runner_id == current_user.id, Run.id == run_id)
+        runs = db.session.query(Run).filter(Run.runner_id == current_user.id, Run.id == run_id).first()
+        prev_run = db.session.query(Run).filter(Run.runner_id == current_user.id, Run.id < run_id).order_by(Run.id.desc()).first()
     else:
         runs = None
-    return render_template("runs.html", run=runs[0])
+        prev_run = None;
+    return render_template("runs.html", run=runs, prev_run=prev_run)
