bdde0d95ed433acd1cf6bea0f0001538044f90ea
Riccardo <paoletti.riccardo0@gmail.com>
2018-11-07 18:52:39 +0100

    Create first test for task 2.2 in test_user_challenge.py

---
+++ tests/test_user_challenge.py
@@ -0,0 +1,32 @@
+from unittest import mock
+from monolith.database import User, Run, Challenge
+from tests.conftest import mocked_result
+
+# Test that checks if the create_user page permits to create 2 user with the same email
+def test_create_same_user(client, db_instance, celery_session_worker):
+    client.post('/create_user', follow_redirects=True,
+        data=dict(
+            submit='Publish',
+            email='1',
+            firstname='1',
+            lastname='1',
+            password='1',
+            age='1',
+            weight='1',
+            max_hr='1',
+            rest_hr='1',
+            vo2max='1'
+        ))
+
+    client.post('/login', follow_redirects=True, data=dict(email='1', password='1'))
+
+    assert db_instance.session.query(Challenge).count() == 0
+
+    resp = client.post('/create_challenge',
+        follow_redirects=True,
+        data=dict(id_run='1'))
+
+    assert resp.status_code == 200
+    
+    assert db_instance.session.query(Challenge).count() == 0
+  
