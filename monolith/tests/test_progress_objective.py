2efc8f0a3d89a48afa05f8420574b35884299c4c
Andrea <andybbruno@gmail.com>
2018-11-10 18:57:02 +0100

    Revert "Progress section tests FINISHED"
    This reverts commit 9962229ddaa657f982f48737deb1d7fcfabee464.

--- monolith/tests/test_progress_objective.py
+++ monolith/tests/test_progress_objective.py
@@ -9,7 +9,6 @@ import random
 def test_objective(client, db_instance):
     
     KILOMETERS = 2
-    OBJ = 10000
 
     # simulate login
     user = ensure_logged_in(client, db_instance)
@@ -42,22 +41,13 @@ def test_objective(client, db_instance):
 
     #Testing remaining kilometers in "PROGRESS" field
     user = db_instance.session.query(User).first()
-    _setObjective(user, OBJ)
+    _setObjective(user, 30000)
     res = client.get("/")
     html=pq(res.data)
     rem_km = float(html("#rem_KM").html())
     objective = float(html("#obj_dist").html())
     total_distance = float(html("#tot_dist").html())
 
+
     assert (rem_km >= 0.0)
     assert rem_km == (objective - total_distance)
-
-
-    #Testing percentage in "PROGRESS" field
-    res = client.get("/")
-    html=pq(res.data)
-    percentage = float(html("#perc").html())
-    objective = float(html("#obj_dist").html())
-    total_distance = float(html("#tot_dist").html())
-
-    assert percentage == (total_distance / objective * 100)
