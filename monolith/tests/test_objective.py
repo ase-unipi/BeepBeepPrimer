a7dab1f8da144b43c6ec073e2e233adc34ce4890
michelecafagna26 <mikele.c@outlook.it>
2018-11-10 21:41:38 +0100

    test

--- monolith/tests/test_objective.py
+++ monolith/tests/test_objective.py
@@ -1,40 +1,46 @@
-from monolith.database import User, Objectives, Run
-from monolith.database import _setObjective
-import random
+from monolith.database import User, Objectives, Run, _setObjective
+from monolith.views.util import km2m
+from pyquery import PyQuery as pq
+from utils import create_mock_user, ensure_logged_in
 
 def test_objective(client, db_instance):
 
-    client.post(
-        '/create_user',
-        data=dict(
-            email='example@test.com',
-            firstname='Jhon',
-            lastname='Doe',
-            password='password',
-            age='22',
-            weight='75',
-            max_hr='150',
-            rest_hr='60',
-            vo2max='10'
-        ),
-        follow_redirects=True
-    )
+    #create fake user
+    create_mock_user(client)
 
+    #retreve the fake user
     user = db_instance.session.query(User).first()
     db_instance.session.add(user)
 
+    #create 2 fake runs
     runs_id = ['1', '2']
-
     for i in runs_id:
         run = Run()
         run.runner = user
         run.strava_id = i
         run.distance = 1000
+        run.average_speed = 10
+        run.elapsed_time= 10000
         db_instance.session.add(run)
 
     db_instance.session.commit()
 
+    #set objective
     objective_distance = 1000
     _setObjective(user, objective_distance)
 
+    #check that the objective is correctly settend in the database
     assert db_instance.session.query(Objectives).first().distance == 1000
+
+    # simulate login
+    user = ensure_logged_in(client, db_instance)
+
+    #check that the objective is correctly showed in the view
+    res = client.get('/')
+    html = pq(res.data)
+
+    objective_view = km2m(html("#obj_dist").html())
+
+    #check the view converting the distance to km
+    assert objective_view == objective_distance
+
--- monolith/tests/utils.py
+++ monolith/tests/utils.py
@@ -1,29 +1,13 @@
 from monolith.database import User
 
-# Creates a new user
-
-
+#log in a user
 def ensure_logged_in(client, db_instance):
 
     user = db_instance.session.query(User).filter(
         User.email == 'example@test.com').first()
 
     if(user is None):
-        client.post(
-            '/create_user',
-            data=dict(
-                email='example@test.com',
-                firstname='Jhon',
-                lastname='Doe',
-                password='password',
-                age='22',
-                weight='75',
-                max_hr='150',
-                rest_hr='60',
-                vo2max='10'
-            ),
-            follow_redirects=True
-        )
+        create_mock_user(client)
 
     # simulate login
     client.post(
@@ -39,3 +23,24 @@ def ensure_logged_in(client, db_instance):
         User.email == 'example@test.com').first()
 
     return user
+
+# Creates a new user
+def create_mock_user(client):
+
+    client.post(
+        '/create_user',
+        data=dict(
+            email='example@test.com',
+            firstname='Jhon',
+            lastname='Doe',
+            password='password',
+            age='22',
+            weight='75',
+            max_hr='150',
+            rest_hr='60',
+            vo2max='10'
+        ),
+        follow_redirects=True
+    )
+
+    return
